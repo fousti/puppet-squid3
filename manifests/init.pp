3b5c755ff3e81dacbd815f7de44a9367d9fed6ef
ismael Tifous <ismael.tifous@monsieurdrive.com>
2015-04-29 17:18:55 +0200

    refactor module for squid version 3.x

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,78 +1,120 @@
-# Class: squid3
+# Define type: squid3
 #
-# Squid 3.x proxy server.
+# Configure and run single squid instance 
+#  
+#  THIS MODULE REQUIRE SQUID 3.4 
+#  HIS ONLY PURPOSE IS TO CONFIGURE AND RUN INSTANCE
 #
-# Sample Usage :
-#     include squid3
 #
-#     class { 'squid3':
-#       acl => [
-#         'de myip 192.168.1.1',
-#         'fr myip 192.168.1.2',
-#         'office src 10.0.0.0/24',
-#       ],
-#       http_access => [
-#         'allow office',
-#       ],
-#       cache => [ 'deny all' ],
-#       via => 'off',
-#       tcp_outgoing_address => [
-#         '192.168.1.1 country_de',
-#         '192.168.1.2 country_fr',
-#       ],
-#       server_persistent_connections => 'off',
-#     }
+# squid3 { "squid-hidden-fr" : 
+#  http_port   => [5000],
+#  cache_dir   => ['null /tmp/squid_hidden/'],
+#  cache_peer  => ['0.0.0.0  parent 5000 0 proxy-only no-query round-robin login=login:pass',
+#          '0.0.0.1  parent 5000 0 proxy-only no-query round-robin login=login:pass',
+#          '0.0.0.2  parent 5000 0 proxy-only no-query round-robin login=login:pass',],
+#  request_header_access => [  'From deny all',
+#                'Server deny all',
+#                'WWW-Authenticate deny all',
+#                'Link deny all',
+#                'Cache-Control deny all',
+#                'Proxy-Connection deny all',
+#                'X-Cache deny all',
+#                'X-Cache-Lookup deny all',
+#                'Via deny all',
+#                'Forwarded-For deny all',
+#                'X-Forwarded-For deny all',
+#                'Pragma deny all',
+#                'Keep-Alive deny all',],
+#  user        => 'mrdrive',
+#  password    => 'toto',
+
+#} 
 #
-class squid3 (
-  # Options are in the same order they appear in squid.conf
-  $http_port            = [ '3128' ],
-  $acl                  = [],
+define squid3 (
+  $http_port            = [],
   $http_access          = [],
-  $icp_access           = [],
-  $tcp_outgoing_address = [],
-  $cache_mem            = '256 MB',
+  $request_header_access = [],
   $cache_dir            = [],
   $cache                = [],
-  $via                  = 'on',
-  $ignore_expect_100    = 'off',
-  $cache_mgr            = 'root',
-  $forwarded_for        = 'on',
-  $client_persistent_connections = 'on',
-  $server_persistent_connections = 'on',
-  $maximum_object_size           = '4096 KB',
-  $maximum_object_size_in_memory = '512 KB',
-  $config_hash                   = {},
-  $template                      = 'long',
-) inherits ::squid3::params {
+  $cache_peer           = [],
+  $via                  = 'off',
+  $forwarded_for        = 'delete',
+  $follow_x_forwarded_for = 'deny all',
+  $config_hash          = {},
+  $user                 = '',
+  $password             = ''
+) {
 
-  $use_template = $template ? {
-    'short' => 'squid3/squid.conf.short.erb',
-    'long'  => 'squid3/squid.conf.long.erb',
-    default => $template,
+  # variable for log file in template 
+
+
+  $access_log = "/var/log/squid/access_${name}.log"                     
+  $cache_store_log = "/var/log/squid/cache_store_${name}.log"         
+  $cache_log = "/var/log/squid/cache_${name}.log"                     
+
+
+
+  #############################
+  httpauth { $user :
+    file     => "/etc/squid/${name}",
+    password => $password,
+    realm => 'realm',
+    mechanism => basic,
+    ensure  => present,
   }
 
-  if ! empty($config_hash) and $use_template == 'long' {
-    fail('$config_hash does not (yet) work with the "long" template!')
+  file { "/etc/squid/${name}" : 
+    owner => proxy,
+    group => proxy,
+    require => Httpauth[$user]
   }
 
+  file { "/var/run/${name}.pid" : 
+    ensure => present,
+    owner  => proxy,
+    group  => proxy,
+    mode   => 0644
+  }
 
-  package { 'squid3_package': ensure => installed, name => $package_name }
+  file { $access_log : 
+    ensure => present,
+    owner  => proxy,
+    group  => proxy,
+    mode   => 0644
+  }
+
+  file { $cache_log : 
+    ensure => present,
+    owner  => proxy,
+    group  => proxy,
+    mode   => 0644
+  }
+
+  file { $cache_store_log : 
+    ensure => present,
+    owner  => proxy,
+    group  => proxy,
+    mode   => 0644
+  }
+
+  file { "/etc/squid/${name}.conf" :
+    notify  => Service['squid3_service'],
+    content => template('squid3/squid.conf.proxy.erb'),
+    owner   => proxy,
+    group   => proxy
+  }
 
   service { 'squid3_service':
     enable    => true,
-    name      => $service_name,
+    name      => $name,
     ensure    => running,
-    restart   => "service ${service_name} reload",
-    path      => ['/sbin', '/usr/sbin'],
-    hasstatus => true,
-    require   => Package['squid3_package'],
+    provider  => base,
+    start     => "squid -f /etc/squid/${name}.conf",
+    path      => ['/sbin', '/usr/sbin', '/usr/local/squid', '/usr/bin/', '/bin'],
+    hasstatus => false,
+    require   => [Httpauth[$user], File["/etc/squid/${name}.conf"], File[$cache_log], File[$access_log], File[$cache_store_log], File["/var/run/${name}.pid"]]
   }
 
-  file { $config_file:
-    require => Package['squid3_package'],
-    notify  => Service['squid3_service'],
-    content => template($use_template),
-  }
 
 }
 
